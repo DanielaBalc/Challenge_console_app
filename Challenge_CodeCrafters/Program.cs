12253f2f72fa3d8ebf7a07d3496df20983637e1b
Balc Daniela <balc.daniela@gmail.com>
2023-06-30 16:15:26 +0300

    update the code

--- Challenge_CodeCrafters/Program.cs
+++ Challenge_CodeCrafters/Program.cs
@@ -2,34 +2,33 @@
 using Challenge_CodeCrafters;
 using Microsoft.VisualBasic;
 
-public void main(string[] args)
+
+
+ string userId;
+DateTime dataTime;
+string password;
+
+Console.WriteLine("Please write your user ID: ");
+userId = Console.ReadLine();
+
+Console.WriteLine("Please write the Date time (2023-06-30 00:00:00): ");
+dataTime = Convert.ToDateTime(Console.ReadLine());
+        
+Password.GeneratePass();
+var timer = new PeriodicTimer(TimeSpan.FromSeconds(30));
+// Console.WriteLine("Time: " + timer.);
+while (await timer.WaitForNextTickAsync())
 {
-
-    string userId;
-    DateTime dataTime;
-    string password;
-
-    Console.WriteLine("Please write your user ID: ");
-    userId = Console.ReadLine();
-
-    Console.WriteLine("Please write the Date time (2023-06-30 00:00:00): ");
-    dataTime = Convert.ToDateTime(Console.ReadLine());
-
-    Password.GeneratePass();
-    var timer = new PeriodicTimer(TimeSpan.FromSeconds(30));
-    // Console.WriteLine("Time: " + timer.);
-    while (await timer.WaitForNextTickAsync())
+    Console.WriteLine("Password has expired. Do you want to generate new one?");
+    string answer = Console.ReadLine();
+    if (answer.ToUpper() == "Y" || answer.ToUpper() == "YES")
     {
-        Console.WriteLine("Password has expired. Do you want to generate new one?");
-        string answer = Console.ReadLine();
-        if (answer.ToUpper() == "Y" || answer.ToUpper() == "YES")
-        {
-            Password.GeneratePass();
-            timer = new PeriodicTimer(TimeSpan.FromSeconds(30));
-        }
-        else
-        {
-            return;
-        }
+        Password.GeneratePass();
+        timer = new PeriodicTimer(TimeSpan.FromSeconds(30));
+    }
+    else
+    {
+        return;
     }
 }
+
