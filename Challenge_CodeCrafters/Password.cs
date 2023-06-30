90cbe8331b6391179e3e5700c6bc41b9307503f8
Balc Daniela <balc.daniela@gmail.com>
2023-06-30 16:04:27 +0300

    Create the console app

---
+++ Challenge_CodeCrafters/Password.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_CodeCrafters
+{
+    class Password
+    {
+        public static void GeneratePass()
+        {
+            string password = "";
+
+            password = GenerateToken();
+
+            Console.WriteLine("Your password will be generated.... ");
+
+            Console.WriteLine("Password: " + password);
+        }
+
+        public static string GenerateToken()
+        {
+            Random random = new Random();
+            int length = random.Next(8, 16) - 4;
+            using (RNGCryptoServiceProvider cryptRNG = new RNGCryptoServiceProvider())
+            {
+                byte[] tokenBuffer = new byte[length];
+                cryptRNG.GetBytes(tokenBuffer);
+                object token = Convert.ToBase64String(tokenBuffer);
+                return token.ToString();
+            }
+        }
+
+       
+    }
+
+}
--- Challenge_CodeCrafters/Program.cs
+++ Challenge_CodeCrafters/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Challenge_CodeCrafters;
+using Microsoft.VisualBasic;
+
+public void main(string[] args)
+{
+
+    string userId;
+    DateTime dataTime;
+    string password;
+
+    Console.WriteLine("Please write your user ID: ");
+    userId = Console.ReadLine();
+
+    Console.WriteLine("Please write the Date time (2023-06-30 00:00:00): ");
+    dataTime = Convert.ToDateTime(Console.ReadLine());
+
+    Password.GeneratePass();
+    var timer = new PeriodicTimer(TimeSpan.FromSeconds(30));
+    // Console.WriteLine("Time: " + timer.);
+    while (await timer.WaitForNextTickAsync())
+    {
+        Console.WriteLine("Password has expired. Do you want to generate new one?");
+        string answer = Console.ReadLine();
+        if (answer.ToUpper() == "Y" || answer.ToUpper() == "YES")
+        {
+            Password.GeneratePass();
+            timer = new PeriodicTimer(TimeSpan.FromSeconds(30));
+        }
+        else
+        {
+            return;
+        }
+    }
+}
